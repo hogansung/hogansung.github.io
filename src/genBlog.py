38882033aa1fdff97771c814d86545c65ee1dad1
Hogan <wrangle1005@gmail.com>
2018-01-06 00:10:24 -0800

    update app_id for Facebook comment system

--- src/genBlog.py
+++ src/genBlog.py
@@ -79,6 +79,9 @@ def getPrefix_article():
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Facebook moderate console -->
+        <meta property="fb:app_id" content="158857908084540"/>
+
         <!-- Bootstrap core CSS -->
         <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
 
